c4c4ae249411e51dfab7cb428dcf6777e830959f
pxf0797 <pxf0797@163.com>
2020-02-03 17:36:52 +0800

    修改日志：200203 pxf 优化调度组件算法 1.调度组件获取最高优先级任务时算法进行简化，提升效率

---
+++ prj/runStuct_v1.4/RteSigs/RteSigs.h
@@ -0,0 +1,33 @@
+/**************************************************************************
+* Copyright    : Copyright(C), 2019, pxf, person.
+* File name    : RteSigs.h
+* Author       : pxf
+* Version      : v1.0
+* Created on   : 2019/12/29 15:41:45
+* Description  : 
+* Others       : 
+* History      : 191229 pxf 初次建立
+***************************************************************************/
+
+#ifndef RTESIGS_H_
+#define RTESIGS_H_
+
+/*头文件包含*/
+#include "../Rte/Rte.h"
+
+/*组件信号*/
+#include "../Abi/AbiSigs.h"
+#include "../CpnSch/CpnSchSigs.h"
+#include "../CpnSigs/Cpn1.h"
+#include "../CpnSigs/Cpn2.h"
+#include "../CpnSigs/Cpn3.h"
+#include "../CpnSigs/Cpn4.h"
+
+
+/*所有信号进行匹配*/
+#include "./RteAsynSigs.h"
+#include "./RteSynSigs.h"
+
+#endif /*RTESIGS_H_*/
+
+/**************************** Copyright(C) pxf ****************************/
